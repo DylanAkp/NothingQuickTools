46ff2a393bb596f650c43675fc0c10936166602c
DylanAkp <59843812+DylanAkp@users.noreply.github.com>
2023-10-20 18:43:13 +0200

    feat(tile): Heads Up Notification

---
+++ app/src/main/java/dev/dylanakp/nothingquicktools/HUNControlTile.kt
@@ -0,0 +1,67 @@
+package dev.dylanakp.nothingquicktools
+
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.InputStreamReader
+
+class HUNControlTile : TileService() {
+
+    override fun onStartListening() {
+        super.onStartListening()
+
+        if (isHUNActive()) {
+            qsTile.state = Tile.STATE_ACTIVE
+            qsTile.updateTile()
+        } else {
+            qsTile.state = Tile.STATE_INACTIVE
+            qsTile.updateTile()
+        }
+    }
+
+    override fun onStopListening() {
+        super.onStopListening()
+    }
+
+    override fun onClick() {
+        super.onClick()
+
+        if (qsTile.state.equals(Tile.STATE_ACTIVE)) {
+            qsTile.state = Tile.STATE_INACTIVE
+            qsTile.updateTile()
+            executeShellCommand("su -c 'settings put global heads_up_notifications_enabled 0'")
+        } else {
+            qsTile.state = Tile.STATE_ACTIVE
+            qsTile.updateTile()
+            executeShellCommand("su -c 'settings put global heads_up_notifications_enabled 1'")
+        }
+
+    }
+
+
+    private fun isHUNActive(): Boolean {
+        val command = "su -c 'settings get global heads_up_notifications_enabled'"
+        val output = executeShellCommand(command)
+        return output == "1"
+    }
+
+    private fun executeShellCommand(command: String): String {
+        val process = Runtime.getRuntime().exec("sh")
+        val outputStream = DataOutputStream(process.outputStream)
+        val inputStream = process.inputStream
+        outputStream.writeBytes("$command\n")
+        outputStream.flush()
+        outputStream.writeBytes("exit\n")
+        outputStream.flush()
+        process.waitFor()
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        val output = StringBuilder()
+        var line: String? = reader.readLine()
+        while (line != null) {
+            output.append(line)
+            line = reader.readLine()
+        }
+        return output.toString()
+    }
+}
